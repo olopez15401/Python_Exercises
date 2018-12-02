6f8ed545f3386d6b99c7e8d075aa319605e1ba19
Oscar <olopez15401@gmail.com>
2018-12-01 18:21:54 -0800

    Adding some more intro programs to Automate the Boring Stuff file

---
+++ AutomateTheBoringStuff/helloSpam.py
@@ -0,0 +1,12 @@
+def printSpam(spam):
+    if spam == 1:
+        print("Hello")
+    elif spam == 2:
+        print("Howdy")
+    else:
+        print("Greetings")
+
+printSpam(4)
+printSpam(2)
+printSpam(9)
+printSpam(1)
---
+++ AutomateTheBoringStuff/oneToTen.py
@@ -0,0 +1,5 @@
+def printToTen():
+    for i in range(1,11):
+        print(str(i))
+
+printToTen()
