6e5120a89e7245e12808184cc65c12baba084107
Oscar <olopez15401@gmail.com>
2018-12-02 19:09:03 -0800

    Some more exercises

---
+++ t_shirt.py
@@ -0,0 +1,6 @@
+def make_shirt(size='Large', printed_message="Shawn hates tabs"):
+    print("The size of this shirt is " + size + " and it prints " +
+          printed_message)
+
+make_shirt('xl','Python 3 or bust')
+make_shirt()
